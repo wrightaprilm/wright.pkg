6ce0b00e0453ad6b4125c9a8b0f7e145a62a8667
April Wright <april.wright@selu.edu>
2023-11-02 19:02:43 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# wright.pkg
+
+## An R package for processing shrubbery data
+
+And type a few words about what your package does.
--- wright.pkg/R/calc_shrub.R
+++
@@ -1,6 +0,0 @@
-calc_den <- function(len, width, height){
-  vol <-  calc_vol(len, width, height)
-  mass <- calc_mass(len)
-  density <- mass/vol
-  return(density)
-}
